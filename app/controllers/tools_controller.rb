ed77a821a71ec8bb8a9137f3fb40104eb51076d7
Norea Dani El Kebir <danielkebirnorea@gmail.com>
2023-05-27 18:04:09 +0200

    en cours

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,2 +1,29 @@
 class ToolsController < ApplicationController
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to tool_path(@tool)
+    else
+      render :new
+    end
+  end
+
+  def index
+   @tools = Tools.all
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:detail, :price)
+  end
 end

75fcf43bbe67a5cd367e87dc7497a2ad856972b8
Norea Dani El Kebir <danielkebirnorea@gmail.com>
2023-06-01 19:44:31 +0200

    en cours

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,38 @@
 class BookingsController < ApplicationController
+  before_action :set_tool, only: [:new, :create]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.tool = @tool
+    if @booking.save
+      redirect_to tool_path(@tool), notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to tool_path(@tool), notice: 'Booking was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_tool
+    @tool = Tool.find(params[:tool_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :tools
+  resources :tools do
+    resources :bookings, only: [:new, :create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
